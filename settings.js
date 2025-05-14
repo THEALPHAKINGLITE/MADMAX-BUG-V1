6ede47dd1f4f3ca04861608c8a26ba5cb10c456b
➣𖤛 ꙰ALPHA-KING ᭡ ꙰𖤛➣ <suhailtechinfo@gmail.com>
2025-05-14 21:27:03 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'MAD-MAX',
+  author: 'ALPHA-KING‎',
+  botName: "MAD-MAX[BUG]-V1",
+  botOwner: 'GLOBALTECHKINGS', // Your name
+  ownerNumber: '263718728504', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for phone crashing[time we pay our old deeds]",
+  version: "1.0.0",
+};
+
+module.exports = settings;
